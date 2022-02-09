c85126eb13028eac7c6cf8ec163d71dce2a2efa0
nakayama hiroyuki <hiroyuki.nakayama@generosity.co.jp>
2022-02-09 10:16:55 +0900

    [add] first

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <input type="text" v-model="message"/><br/>
+    <input type="text" v-model="message2"/>
+    <HelloWorld v-bind:msg="message" v-on:update="updateMessage"/>
+    <HelloWorld :msg="message2" @update="updateMessage2"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld
+  },
+  setup () {
+    const message = ref<string>()
+    message.value = 'hello'
+    const message2 = ref<string>()
+    message2.value = 'hello hello'
+
+    const updateMessage = (val:string) => {
+      message2.value = val
+    }
+    const updateMessage2 = (val:string) => {
+      message.value = val
+    }
+    return {
+      message,
+      message2,
+      updateMessage,
+      updateMessage2
+    }
   }
 })
 </script>
